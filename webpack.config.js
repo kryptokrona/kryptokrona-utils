713678292745c1b76bbefa99bf27e405cd5be5b0
Brandon Lehmann <brandonlehmann@gmail.com>
2019-06-01 12:45:36 -0400

    Update to copy the the .mem and wasm files to the dist folder on webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
+'use strict'
+
+const CopyWebPackPlugin = require('copy-webpack-plugin')
+
 module.exports = {
   mode: 'production',
   entry: './index.js',
@@ -9,5 +13,12 @@ module.exports = {
   node: {
     fs: 'empty'
   },
-  target: 'web'
+  target: 'web',
+  plugins: [
+    new CopyWebPackPlugin([
+      { from: 'lib/turtlecoin-crypto/turtlecoin-crypto.js.mem', to: 'turtlecoin-crypto.js.mem' },
+      { from: 'lib/turtlecoin-crypto/turtlecoin-crypto-wasm.js', to: 'turtlecoin-crypto-wasm.js' },
+      { from: 'lib/turtlecoin-crypto/turtlecoin-crypto-wasm.wasm', to: 'turtlecoin-crypto-wasm.wasm' }
+    ])
+  ]
 }
