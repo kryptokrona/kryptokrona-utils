b4c6ddfdc67e5c736ec54f3f35a15b207abddb2d
Brandon Lehmann <brandonlehmann@gmail.com>
2019-09-23 09:17:18 -0400

    add additional cryptography tests

--- tests/tests.js
+++ tests/tests.js
@@ -8,10 +8,73 @@ const assert = require('assert')
 const Crypto = require('../').Crypto
 const describe = require('mocha').describe
 const it = require('mocha').it
+const TurtleCoinCrypto = new Crypto()
 const TurtleCoinUtils = require('../').CryptoNote
 const cnUtil = new TurtleCoinUtils(require('../config.json'))
 
-console.log('Using Crypto: %s', (new Crypto()).type)
+console.log('Using Crypto: %s', TurtleCoinCrypto.type)
+
+describe('Cryptography', () => {
+  const testdata = '0100fb8e8ac805899323371bb790db19218afd8db8e3755d8b90f39b3d5506a9abce4fa912244500000000ee8146d49fa93ee724deb57d12cbc6c6f3b924d946127c7a97418f9348828f0f02'
+
+  it('Tree Hash', () => {
+    const expectedTreeHash = 'dff9b4e047803822e97fb25bb9acb8320648954e15a6ddf6fa757873793c535e'
+    const [err, treeHash] = TurtleCoinCrypto.tree_hash([
+      'b542df5b6e7f5f05275c98e7345884e2ac726aeeb07e03e44e0389eb86cd05f0',
+      '1b606a3f4a07d6489a1bcd07697bd16696b61c8ae982f61a90160f4e52828a7f',
+      'c9fae8425d8688dc236bcdbc42fdb42d376c6ec190501aa84b04a4b4cf1ee122',
+      '871fcd6823f6a879bb3f33951c8e8e891d4043880b02dfa1bb3be498b50e7578'
+    ])
+    assert(treeHash === expectedTreeHash && !err)
+  })
+
+  it('Tree Branch', () => {
+    const expectedTreeBranch = [
+      'f49291f9b352701d97dffad838def8cefcc34d1e767e450558261b161ab78cb1',
+      '1b606a3f4a07d6489a1bcd07697bd16696b61c8ae982f61a90160f4e52828a7f'
+    ]
+
+    const [err, treeBranch] = TurtleCoinCrypto.tree_branch([
+      'b542df5b6e7f5f05275c98e7345884e2ac726aeeb07e03e44e0389eb86cd05f0',
+      '1b606a3f4a07d6489a1bcd07697bd16696b61c8ae982f61a90160f4e52828a7f',
+      'c9fae8425d8688dc236bcdbc42fdb42d376c6ec190501aa84b04a4b4cf1ee122',
+      '871fcd6823f6a879bb3f33951c8e8e891d4043880b02dfa1bb3be498b50e7578'
+    ])
+
+    assert(!err)
+    assert.deepStrictEqual(treeBranch, expectedTreeBranch)
+  })
+
+  const algos = [
+    { name: 'CryptoNight Fast Hash', func: 'cn_fast_hash', hash: 'b542df5b6e7f5f05275c98e7345884e2ac726aeeb07e03e44e0389eb86cd05f0' },
+    { name: 'CryptoNight v0', func: 'cn_slow_hash_v0', hash: '1b606a3f4a07d6489a1bcd07697bd16696b61c8ae982f61a90160f4e52828a7f' },
+    { name: 'CryptoNight v1', func: 'cn_slow_hash_v1', hash: 'c9fae8425d8688dc236bcdbc42fdb42d376c6ec190501aa84b04a4b4cf1ee122' },
+    { name: 'CryptoNight v2', func: 'cn_slow_hash_v2', hash: '871fcd6823f6a879bb3f33951c8e8e891d4043880b02dfa1bb3be498b50e7578' },
+    { name: 'CryptoNight Lite v0', func: 'cn_lite_slow_hash_v0', hash: '28a22bad3f93d1408fca472eb5ad1cbe75f21d053c8ce5b3af105a57713e21dd' },
+    { name: 'CryptoNight Lite v1', func: 'cn_lite_slow_hash_v1', hash: '87c4e570653eb4c2b42b7a0d546559452dfab573b82ec52f152b7ff98e79446f' },
+    { name: 'CryptoNight Lite v2', func: 'cn_lite_slow_hash_v2', hash: 'b7e78fab22eb19cb8c9c3afe034fb53390321511bab6ab4915cd538a630c3c62' },
+    { name: 'CryptoNight Dark v0', func: 'cn_dark_slow_hash_v0', hash: 'bea42eadd78614f875e55bb972aa5ec54a5edf2dd7068220fda26bf4b1080fb8' },
+    { name: 'CryptoNight Dark v1', func: 'cn_dark_slow_hash_v1', hash: 'd18cb32bd5b465e5a7ba4763d60f88b5792f24e513306f1052954294b737e871' },
+    { name: 'CryptoNight Dark v2', func: 'cn_dark_slow_hash_v2', hash: 'a18a14d94efea108757a42633a1b4d4dc11838084c3c4347850d39ab5211a91f' },
+    { name: 'CryptoNight Dark Lite v0', func: 'cn_dark_lite_slow_hash_v0', hash: 'faa7884d9c08126eb164814aeba6547b5d6064277a09fb6b414f5dbc9d01eb2b' },
+    { name: 'CryptoNight Dark Lite v1', func: 'cn_dark_lite_slow_hash_v1', hash: 'c75c010780fffd9d5e99838eb093b37c0dd015101c9d298217866daa2993d277' },
+    { name: 'CryptoNight Dark Lite v2', func: 'cn_dark_lite_slow_hash_v2', hash: 'fdceb794c1055977a955f31c576a8be528a0356ee1b0a1f9b7f09e20185cda28' },
+    { name: 'CryptoNight Turtle v0', func: 'cn_turtle_slow_hash_v0', hash: '546c3f1badd7c1232c7a3b88cdb013f7f611b7bd3d1d2463540fccbd12997982' },
+    { name: 'CryptoNight Turtle v1', func: 'cn_turtle_slow_hash_v1', hash: '29e7831780a0ab930e0fe3b965f30e8a44d9b3f9ad2241d67cfbfea3ed62a64e' },
+    { name: 'CryptoNight Turtle v2', func: 'cn_turtle_slow_hash_v2', hash: 'fc67dfccb5fc90d7855ae903361eabd76f1e40a22a72ad3ef2d6ad27b5a60ce5' },
+    { name: 'CryptoNight Turtle Lite v0', func: 'cn_turtle_lite_slow_hash_v0', hash: '5e1891a15d5d85c09baf4a3bbe33675cfa3f77229c8ad66c01779e590528d6d3' },
+    { name: 'CryptoNight Turtle Lite v1', func: 'cn_turtle_lite_slow_hash_v1', hash: 'ae7f864a7a2f2b07dcef253581e60a014972b9655a152341cb989164761c180a' },
+    { name: 'CryptoNight Turtle Lite v2', func: 'cn_turtle_lite_slow_hash_v2', hash: 'b2172ec9466e1aee70ec8572a14c233ee354582bcb93f869d429744de5726a26' },
+    { name: 'Chukwa', func: 'chukwa_slow_hash', hash: 'c0dad0eeb9c52e92a1c3aa5b76a3cb90bd7376c28dce191ceeb1096e3a390d2e' }
+  ]
+
+  algos.forEach((algo) => {
+    it(algo.name, () => {
+      const [err, hash] = TurtleCoinCrypto[algo.func](testdata)
+      assert(algo.hash === hash && !err)
+    })
+  })
+})
 
 describe('Wallets', () => {
   const rawSeed = 'dd0c02d3202634821b4d9d91b63d919725f5c3e97e803f3512e52fb0dc2aab0c'
@@ -214,16 +277,6 @@ describe('Transactions', () => {
         assert(e === false)
       }
     })
-  })
-})
-
-describe('Hashing', () => {
-  it('CryptoNight Fast Hash', () => {
-    const inputData = '0100fb8e8ac805899323371bb790db19218afd8db8e3755d8b90f39b3d5506a9abce4fa912244500000000ee8146d49fa93ee724deb57d12cbc6c6f3b924d946127c7a97418f9348828f0f02'
-    const expectedHash = 'b542df5b6e7f5f05275c98e7345884e2ac726aeeb07e03e44e0389eb86cd05f0'
-    const calculatedHash = cnUtil.cnFastHash(inputData)
-
-    assert(expectedHash === calculatedHash)
   })
 })
 
